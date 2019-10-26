375c2105450c5c26f37db70724f4fbeda5de5dcb
SukkaW <isukkaw@gmail.com>
2019-10-26 09:52:09 +0800

    Finish General

---
+++ src/js/util.js
@@ -0,0 +1,21 @@
+function Modal(title = '', body = '', footer = '') {
+    document.getElementById('modal-title').innerHTML = title;
+    document.getElementById('modal-body').innerHTML = body;
+    if (footer !== '') {
+        document.getElementById('modal-footer').innerHTML = footer;
+    }
+    $('#modal').modal();
+}
+
+function setLS(key, value) {
+    try {
+        localStorage.setItem(key, value)
+    } catch (o) {
+        Modal(
+            '这看起来不太正常',
+            `<p>你的浏览器不支持 localStorage，因此无法继续使用 ClashEditor</p>
+            <p>报错信息如下所示：</p>
+            <p><code>${o}</code></p>`
+        )
+    }
+}
